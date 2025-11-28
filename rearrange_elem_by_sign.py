ebd40f15f98f5b691855b7d79dedf70c3f3d0ac9
s-atharva <atharvasarbhukan@gmail.com>
2025-11-28 15:55:20 +0530

    Add Rearrange Elem

--- rearrange_elem_by_sign.py
+++ rearrange_elem_by_sign.py
@@ -18,5 +18,47 @@ def rearrange_elem_by_sign(arr: List[int]) -> List[int]:
     return arr
 
 
-my_array = [3, 1, - 2, -5, 2, -4]
-print(rearrange_elem_by_sign(arr=my_array))
+def rearrange_elem_by_sign_optimal(arr: List[int]) -> List[int]:
+    result = [0] * len(arr)
+    positive = 0
+    negative = 1
+
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            result[positive] = arr[i]
+            positive += 2
+        else:
+            result[negative] = arr[i]
+            negative += 2
+    return result
+
+
+def rearrange_elem_by_sign_version_2(arr: List[int]) -> List[int]:
+    result = [0] * len(arr)
+    positive_arr = []
+    negative_arr = []
+    for elem in arr:
+        if elem > 0:
+            positive_arr.append(elem)
+        else:
+            negative_arr.append(elem)
+    if len(positive_arr) > len(negative_arr):
+        for i in range(0, len(negative_arr)):
+            result[2 * i] = positive_arr[i]
+            result[2 * i + 1] = negative_arr[i]
+        for i in range(len(negative_arr), len(positive_arr)):
+            result[len(negative_arr) * 2] = positive_arr[i]
+    else:
+        for i in range(0, len(positive_arr)):
+            result[2 * i] = positive_arr[i]
+            result[2 * i + 1] = negative_arr[i]
+        for i in range(len(negative_arr), len(positive_arr)):
+            result[len(positive_arr) * 2] = negative_arr[i]
+
+    return result
+
+
+my_array = [-1, 2, 3, 4, -3, 1]
+# print(rearrange_elem_by_sign(arr=my_array))
+# print(rearrange_elem_by_sign_optimal(arr=my_array))
+print(rearrange_elem_by_sign_version_2(arr=my_array))
