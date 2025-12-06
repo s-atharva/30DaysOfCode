9ac05a186a5702c7ca28a826831d547e31857030
s-atharva <atharvasarbhukan@gmail.com>
2025-12-06 23:15:04 +0530

    Add longest consecutive seq

--- longest_consective_seq.py
+++ longest_consective_seq.py
@@ -1,21 +1,18 @@
 from typing import List
 
 
-def find_longest_consecutive_seq(arr: List[int]) -> int:
+def find_long_consecutive_seq_brute(arr: List[int]) -> int:
     longest = 0
-    pass
-    for num in arr:
-        current = num
-        length = 1
-
-        while (current + 1) in arr:
+    for elem in arr:
+        current = elem
+        current_count = 1
+        while current + 1 in arr:
             current += 1
-            length += 1
-
-        longest = max(longest, length)
+            current_count += 1
+        longest = max(longest, current_count)
 
     return longest
 
 
-my_arr = [102, 4, 100, 1, 101, 3, 2, 1, 1]
-print(find_longest_consecutive_seq(arr=my_arr))
+my_arr = [102, 4, 100, 1, 102, 3, 2, 1, 1]
+print(find_long_consecutive_seq_brute(arr=my_arr))
