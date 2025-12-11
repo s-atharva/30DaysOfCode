eed22c3d1f8a970dda7de3a8d0c255e83e18147f
s-atharva <atharvasarbhukan@gmail.com>
2025-12-11 23:38:50 +0530

    Add rotate matrix 90

--- rotate_matrix_by_90.py
+++ rotate_matrix_by_90.py
@@ -11,5 +11,5 @@ def rotate_matrix_90(matrix: List[List[int]]) -> List[List[int]]:
     return matrix
 
 
-matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 18]]
 print(rotate_matrix_90(matrix=matrix))
