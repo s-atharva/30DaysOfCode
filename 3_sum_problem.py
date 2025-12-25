cca78d3ed1105d2ff8e49741eb6f1691c23708a8
s-atharva <atharvasarbhukan@gmail.com>
2025-12-25 18:03:52 +0530

    Add 3 sum problem

--- 3_sum_problem.py
+++ 3_sum_problem.py
@@ -1,20 +1,20 @@
 from typing import List
 
 
-def find_3_sum(arr: List[int]) -> List[tuple]:
+def find_3_sum(arr: List[int]):
     n = len(arr)
     my_set = set()
     for i in range(0, n):
         for j in range(i + 1, n):
             for k in range(j + 1, n):
                 if arr[i] + arr[j] + arr[k] == 0:
-                    temp = [arr[i], arr[j], arr[k]]
+                    temp = list((arr[i], arr[j], arr[k]))
                     temp.sort()
                     my_set.add(tuple(temp))
-    return list(my_set)
+    return [list(item) for item in my_set]
 
 
-def find_3_sum_2_approach(arr: List[int]) -> list[tuple]:
+def find_3_sum_with_two_loops(arr: List[int]):
     n = len(arr)
     result = set()
     for i in range(0, n):
@@ -22,13 +22,14 @@ def find_3_sum_2_approach(arr: List[int]) -> list[tuple]:
         for j in range(i + 1, n):
             third = -(arr[i] + arr[j])
             if third in my_set:
-                temp = [arr[i], arr[j], third]
+                temp = list((arr[i], arr[j], third))
                 temp.sort()
                 result.add(tuple(temp))
-            my_set.add(arr[j])
-    return list(result)
+            else:
+                my_set.add(arr[j])
+    return [[item] for item in result]
 
 
-my_arr = [-1, 0, 1, 2, -1, 4]
-print(find_3_sum(arr=my_arr))
-print(find_3_sum_2_approach(arr=my_arr))
+my_arr = [-1, 0, 1, 2, -1, -4]
+# print(find_3_sum(arr=my_arr))
+print(find_3_sum_with_two_loops(arr=my_arr))
