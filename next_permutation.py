2d9640298c7d283702ce358b74569a159f45d057
s-atharva <atharvasarbhukan@gmail.com>
2025-12-27 18:05:59 +0530

    Add next permutation problem

---
+++ next_permutation.py
@@ -0,0 +1,15 @@
+# from typing import List
+#
+#
+# def next_permutation(nums: List[int]) -> List[int]:
+#     n = len(nums)
+#     i = n - 2
+#     while i >= 0 and nums[i] >= nums[i + 1]:
+#         i -= 1
+#     if i >= 0:
+#         j = n - 1
+#         while nums[j]
+#
+#
+# my_arr = [1, 5, 2, 3]
+# print(next_permutation(nums=my_arr))
