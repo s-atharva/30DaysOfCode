e6143150deb8b443902d373efc565719387fc97e
s-atharva <atharvasarbhukan@gmail.com>
2026-01-04 12:07:40 +0530

    Add lower upper bound problem

--- lower_upper_bound.py
+++ lower_upper_bound.py
@@ -1,39 +1,38 @@
 from typing import List
 
 
-def find_lower_bound(nums: List[int], target):
-    start = 0
-    end = len(nums) - 1
+def find_lower_bound(nums: List[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
     lower_bound = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if target == arr[mid]:
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
             lower_bound = mid
-            end = mid - 1
-        elif target <= arr[mid]:
-            end = mid - 1
+            high = mid - 1
+        elif target <= nums[mid]:
+            high = mid - 1
         else:
-            start = mid + 1
+            low = mid + 1
     return lower_bound
 
 
-def find_upper_bound(nums: List[int], target):
-    start = 0
-    end = len(nums) - 1
+def find_upper_bound(nums: List[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
     upper_bound = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if target == nums[mid]:
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
             upper_bound = mid
-            start = mid + 1
+            low = mid + 1
         elif target <= nums[mid]:
-            end = mid - 1
+            high = mid - 1
         else:
-            start = mid + 1
+            low = mid + 1
     return upper_bound
 
 
-arr = [1, 1, 1, 2, 3, 3, 5, 6, 7, 7, 7, 9, 12, 12, 13]
-lower = find_lower_bound(nums=arr, target=7)
-higher = find_upper_bound(nums=arr, target=7)
-print(higher - lower + 1)
+nums_list = [2, 4, 10, 10, 10, 18, 20]
+print(find_lower_bound(nums=nums_list, target=10))
+print(find_upper_bound(nums=nums_list, target=10))
