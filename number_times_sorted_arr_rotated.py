2da2d1d501963c16afb02eba00923873cff5116f
s-atharva <atharvasarbhukan@gmail.com>
2026-01-10 17:35:13 +0530

    Add number timest sorted array rotate

--- number_times_sorted_arr_rotated.py
+++ number_times_sorted_arr_rotated.py
@@ -6,16 +6,14 @@ def find_sorted_times(arr: List[int]) -> int:
     start = 0
     end = n - 1
     while start <= end:
-        # if array is sorted
         if arr[start] <= arr[end]:
             return start
-        # if array is not sorted
         mid = (start + end) // 2
         prev = (mid - 1 + n) % n
         nxt = (mid + 1) % n
-        if arr[prev] > arr[mid] and arr[nxt] > arr[mid]:
+        if arr[prev] >= arr[mid] and arr[nxt] >= arr[mid]:
             return mid
-        if arr[start] < arr[mid]:
+        if arr[start] <= arr[mid]:
             start = mid + 1
         else:
             end = mid - 1
