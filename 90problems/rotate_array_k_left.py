9143c53419de029bf882ce69903206f30a0d67a0
s-atharva <atharvasarbhukan@gmail.com>
2026-04-04 14:32:01 +0530

    Add rotate array by k left

--- 90problems/rotate_array_k_left.py
+++ 90problems/rotate_array_k_left.py
@@ -15,5 +15,24 @@ def rotate_array_k_left(arr, k):
     return arr
 
 
+def reverse(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+
+def reverse_left_k(arr, k):
+    n = len(arr)
+    k = k % n
+
+    reverse(arr, 0, k - 1)
+    reverse(arr, k, n - 1)
+    reverse(arr, 0, n - 1)
+
+    return arr
+
+
 my_array = [1, 2, 3, 4, 5, 6, 7]
-print(rotate_array_k_left(arr=my_array, k=13))
+# print(rotate_array_k_left(arr=my_array, k=3))
+print(reverse_left_k(arr=my_array, k=3))
