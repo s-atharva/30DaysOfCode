ca8241518b59ca81891428b37f14051cd041307b
s-atharva <atharvasarbhukan@gmail.com>
2026-05-28 10:12:39 +0530

    Add pivot probelm

--- prefix_sum/count_subarray_sum_k.py
+++ prefix_sum/count_subarray_sum_k.py
@@ -1,15 +1,24 @@
 def count_subarray(arr, k):
+    prefix_sum = 0
     count = 0
-    n = len(arr)
-    for i in range(0, n):
-        total = 0
-        for j in range(i, n):
-            total += arr[j]
-            if total == k:
-                count += 1
+
+    frequency = {0: 1}
+
+    for num in arr:
+        prefix_sum += num
+
+        need = prefix_sum - k
+
+        if need in frequency:
+            count += frequency[need]
+
+        if prefix_sum not in frequency:
+            frequency[prefix_sum] = 1
+        else:
+            frequency[prefix_sum] += 1
 
     return count
 
 
-my_arr = [1, 2, 3, 4]
-print(count_subarray(arr=my_arr, k=5))
+my_arr = [1, 2, 3, -3, 1, 1, 1]
+print(count_subarray(arr=my_arr, k=3))
--- prefix_sum/pivot_value.py
+++ prefix_sum/pivot_value.py
@@ -3,14 +3,39 @@ from typing import List
 
 def find_pivot(arr: List[int]) -> int:
     n = len(arr)
-    for i in range(n):
-        left_sum = sum(arr[0:i])
-        right_sum = sum(arr[i + 1:n])
+    total = 0
+    for elem in arr:
+        total += elem
+    left = 0
+    for pivot in range(n):
+        right = total - left - arr[pivot]
+        if left == right:
+            return pivot
+        left += arr[pivot]
+    return -1
 
-        if left_sum == right_sum:
-            return arr[i]
+
+def pivot_prefix_suffix(arr):
+    n = len(arr)
+    prefix = [0] * n
+    prefix[0] = arr[0]
+
+    suffix = [0] * n
+    suffix[n - 1] = arr[n - 1]
+
+    for i in range(n):
+        prefix[i] = prefix[i - 1] + arr[i]
+
+    for i in range(n - 2, -1, -1):
+        suffix[i] = suffix[i + 1] + arr[i]
+
+    for i in range(n):
+        if prefix[i] == suffix[i]:
+            return i
+
     return -1
 
 
 my_array = [-7, 1, 5, 2, -4, 3, 0]
 print(find_pivot(arr=my_array))
+# print(pivot_prefix_suffix(arr=my_array))
