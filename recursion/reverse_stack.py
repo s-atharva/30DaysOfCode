1e51dd73df0cf3ec6efd7f9329d709668cd625e0
s-atharva <atharvasarbhukan@gmail.com>
2026-07-09 18:05:25 +0530

    Add reverse_stack problem

--- recursion/reverse_stack.py
+++ recursion/reverse_stack.py
@@ -1,13 +1,42 @@
-def reverse_stack(arr):
-    # base case
+# def reverse_stack_loop(arr):
+#     reversed_arr = []
+#     while len(arr) > 0:
+#         reversed_arr.append(arr.pop())
+#     return reversed_arr
+
+
+def reverse_stack(arr, reversed_arr):
+    if len(arr) == 0:
+        return None
+    # pop last elem
+    temp = arr.pop()
+    # induction step
+    reversed_arr.append(temp)
+    # hypothesis
+    reverse_stack(arr, reversed_arr)
+
+
+def reverse_stack_without_space(arr):
     if len(arr) == 1:
         return None
     temp = arr.pop()
-    reverse_stack(arr)
-    arr.append(temp)
+    reverse_stack_without_space(arr)
+    insert_front(arr, temp)
 
 
-my_arr = [5, 4, 3, 2, 1]
+def insert_front(arr, temp):
+    if len(arr) == 0:
+        arr.append(temp)
+        return None
+    val = arr.pop()
+    insert_front(arr, temp)
+    arr.append(val)
+
+
+my_arr = [1, 2, 3, 4, 5]
+reversed_arr = []
+# print(reverse_stack_loop(my_arr))
+# reverse_stack(my_arr, reversed_arr)
+reverse_stack_without_space(my_arr)
 print(my_arr)
-reverse_stack(my_arr)
-print(my_arr)
+# print(reversed_arr)
