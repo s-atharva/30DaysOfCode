84c3f7cdd68b965781cae85e68048d6990df7c74
s-atharva <atharvasarbhukan@gmail.com>
2026-08-02 17:26:08 +0530

    Add rearrange 0 and 1 problem

---
+++ two_pointers/rearrange_0_1.py
@@ -0,0 +1,17 @@
+def rearrange(arr):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        if arr[left] == 0:
+            left += 1
+        elif arr[right] == 1:
+            right -= 1
+        else:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+    return arr
+
+
+my_arr = [1, 0, 1, 0, 1, 0]
+print(rearrange(my_arr))
