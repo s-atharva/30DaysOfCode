cfb482cf83f17c1f63d395483df296688ad3364c
s-atharva <atharvasarbhukan@gmail.com>
2026-08-08 19:11:18 +0530

    Add two sum problem

--- two_pointers/two_sum.py
+++ two_pointers/two_sum.py
@@ -1,24 +1,19 @@
 def two_sum(arr, target):
-    nums = []
-    n = len(arr)
-    for i in range(n):
-        nums.append((arr[i], i))
+    # for i in range(len(arr)):
+    #     for j in range(i + 1, len(arr)):
+    #         if arr[i] + arr[j] == target:
+    #             return [i, j]
+    # return []
 
-    nums.sort()
-
-    left = 0
-    right = n - 1
-
-    while left < right:
-        total = nums[left][0] + nums[right][0]
-        if total == target:
-            return [nums[left][1], nums[right][1]]
-        elif total < target:
-            left += 1
+    # TC -> O(n) and SC -> O(n)
+    my_dict = {}
+    for i in range(len(arr)):
+        need = target - arr[i]
+        if need in my_dict:
+            return [my_dict[need], i]
         else:
-            right -= 1
-    return []
+            my_dict[arr[i]] = i
 
 
 my_arr = [3, 2, 4]
-two_sum(arr=my_arr, target=6)
+print(two_sum(arr=my_arr, target=6))
